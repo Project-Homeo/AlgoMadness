01e72b9abec48ccbaba758ce79d9fd50da7b3c93
rajashreegh <46857948+rajashreegh@users.noreply.github.com>
2022-10-11 11:14:48 +0530

    multiplication of two numbers

---
+++ Multi.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Multi{
+    public static void main(String[] args){
+        int a = 5;
+        int b = 6;
+        System.out.printf("Multiplication of %d and %d is: %d",a,b,a*b);
+    }
+}
